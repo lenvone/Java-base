61a2667764058399a65500cddc0eac9a156c6366
@nonsense-output-machine <3381228856@qq.com>
2023-08-28 00:06:44 +0800

    快排

--- src/com/adam/base/branch/quickSort.java
+++ src/com/adam/base/branch/quickSort.java
@@ -10,26 +10,33 @@ import java.util.Arrays;
  * @date 2023/8/15
  */
 public class quickSort {
-    public static void quickSort(int[] arr){
-        quickSort(arr,0,arr.length-1);
+
+    public static void main(String[] args) {
+        /*
+        https://blog.csdn.net/shujuelin/article/details/82423852
+         */
+        int[] arr = {6, 1, 2, 7, 9, 11, 4, 5, 10, 8};
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
     }
-    public static void quickSort(int[] arr,int start,int end){
-        if (start > end){
+
+    public static void quickSort(int[] arr, int start, int end){
+        // 递归结束条件
+        if (start >= end){
             return;
         }
 
-        // 基准值
         int base = arr[start];
         int left = start;
         int right = end;
 
-        // 递归结束条件
         while(left < right){
 
-            while(left < right && arr[right] >= base){
+            while (left < right && arr[right] >= base){
                 right --;
             }
-            while (left < right && arr[left] <= base){
+            while(left < right && arr[left] <= base){
                 left ++;
             }
 
@@ -37,20 +44,15 @@ public class quickSort {
             arr[left] = arr[right];
             arr[right] = temp;
         }
+
         // 交换基准值的位置
         arr[start] = arr[left];
         arr[left] = base;
+
         // 对左边元素进行快排
-        quickSort(arr,start,left - 1);
-        // 对右边的元素进行快排
-        quickSort(arr,left + 1,end);
+        quickSort(arr, start, left - 1);
+        // 对右边元素进行快排
+        quickSort(arr, left + 1, end);
     }
 
-    public static void main(String[] args) {
-        int[] arr = {2,6,4,8,9,5,1,7};
-
-        quickSort(arr);
-
-        System.out.println(Arrays.toString(arr));
-    }
 }
